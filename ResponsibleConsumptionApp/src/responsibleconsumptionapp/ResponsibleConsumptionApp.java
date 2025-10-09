bdcada1bed578b4e695a327b030875636a289300
lmoares <moares.lorenzo@gmail.com>
2025-10-09 08:55:22 +0100

    first commit

---
+++ ResponsibleConsumptionApp/src/responsibleconsumptionapp/ResponsibleConsumptionApp.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package responsibleconsumptionapp;
+
+/*
+ * Classname ResponsibleConsumptionApp.java
+ * Date 09/10/2025
+ * @author Lorenzo Moares Nunez, 23378441
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ResponsibleConsumptionApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+
+}
