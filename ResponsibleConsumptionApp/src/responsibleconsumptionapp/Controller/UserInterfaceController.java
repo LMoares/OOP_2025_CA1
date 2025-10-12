ab7f70549c41a18183a552b12f0055457bcefba9
lmoares <moares.lorenzo@gmail.com>
2025-10-12 15:19:28 +0100

    defined basic ui structure and placeholder ui interaction - 12/10/2025

--- ResponsibleConsumptionApp/src/responsibleconsumptionapp/Controller/UserInterfaceController.java
+++ ResponsibleConsumptionApp/src/responsibleconsumptionapp/Controller/UserInterfaceController.java
@@ -5,11 +5,29 @@
 
 package responsibleconsumptionapp.Controller;
 
+import responsibleconsumptionapp.View.UserInterfaceView;
+
 /*
  * Classname UserInterfaceController.java
  * Date 11/10/2025
  * @author Lorenzo Moares Nunez, 23378441
  */
 public class UserInterfaceController {
-
+    private UserInterfaceView ui;
+    
+    public UserInterfaceController() {
+        ui = new UserInterfaceView();
+        ui.generateNavbar();
+        ui.generateCards();
+    }
+    
+    public void show(){
+        ui.setVisible(true);
+    }
+    
+    public void repaint(){
+        ui.repaint();
+        ui.revalidate();
+    }
+    
 }
